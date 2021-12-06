4813db23e9969a7ac7c8e85c90d3e20f5c32635b
bibekanandmohanty <vivek@imprintnext.com>
2021-12-06 05:54:37 +0000

    third commit

--- dbcreation.php
+++ dbcreation.php
@@ -3,10 +3,6 @@ $server="localhost";
 $username = "root";
 $pwd="";
 $con = mysqli_connect($server,$username,$pwd);
-$con = mysqli_connect($server,$username,$pwd);
-$con = mysqli_connect($server,$username,$pwd);
-$con = mysqli_connect($server,$username,$pwd);
-$con = mysqli_connect($server,$username,$pwd);
 
 if(!$con){
 echo"Connection failed".mysqli_error($con);
